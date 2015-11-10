bbbccdaa5798dabdef5e4ba9e9ef1b8d1f335319
unknown <andreandyp@outlook.com>
2015-11-10 10:12:09 -0600

    Ya quedo, esperamos que le guste a Mosso

--- main.c
+++ main.c
@@ -4,14 +4,37 @@ void residuos(base,numero);
 int valores[],i;
 int main() {
 	int base,numero,j;
-	printf("Inserta un numero en base 10");
+	printf("Inserta un numero en base 10: ");
 	scanf("%d",&numero);
-	printf("Inserta la nueva base");
+	printf("Inserta la nueva base: ");
 	scanf("%d",&base);
 	residuos(base,numero);
 	j = i;
+	printf("El valor en la base seleccionada es: ");
 	while(j > 0){
-		printf("%d",valores[j-1]);
+		switch(valores[j-1]){
+			case 10:
+				printf("%c",'A');
+				break;
+			case 11:
+				printf("%c",'B');
+				break;
+			case 12:
+				printf("%c",'C');
+				break;
+			case 13:
+				printf("%c",'D');
+				break;
+			case 14:
+				printf("%c",'E');
+				break;
+			case 15:
+				printf("%c",'F');
+				break;
+			default:
+				printf("%d",valores[j-1]);
+				break;
+		}
 		j--;
 	}
 }
